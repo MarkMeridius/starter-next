30541241e3465f6329e04563f1afbb9d17924d21
MarkMeridius <109876299+MarkMeridius@users.noreply.github.com>
2022-07-23 22:13:41 +0100

    changed text

--- apps/starter/pages/index.tsx
+++ apps/starter/pages/index.tsx
@@ -2,7 +2,7 @@ export function Index() {
   return (
     <div className="grid place-items-center w-screen  h-screen bg-black">
       <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-700 to-pink-600 text-8xl  font-black">
-        Yes Sir
+        what the fuck
       </div>
     </div>
   );
